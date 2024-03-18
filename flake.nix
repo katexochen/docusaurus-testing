bfa46644f46853f1bb944d539206b0a87fe296cf
Paul Meyer <49727155+katexochen@users.noreply.github.com>
2024-03-18 12:58:34 +0100

    package
    Signed-off-by: Paul Meyer <49727155+katexochen@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -12,6 +12,31 @@
       inherit (pkgs) lib;
     in
     {
+      packages.${system}.default = pkgs.mkYarnPackage
+        rec {
+          pname = "my-website";
+          version = "0.1.0";
+          src = ./my-website;
+          packageJSON = "${src}/package.json";
+          offlineCache = pkgs.fetchYarnDeps {
+            yarnLock = "${src}/yarn.lock ";
+            hash = "sha256-cdqM2AkGzgiuoHAyWmq/btbubJkYqBCSlPCAOgMSEZ0=";
+          };
+          configurePhase = ''
+            cp -r $node_modules node_modules
+            chmod +w node_modules
+          '';
+          buildPhase = ''
+            export HOME=$(mktemp -d)
+            yarn --offline build
+          '';
+          distPhase = "true";
+          installPhase = ''
+            mkdir -p $out
+            cp -R build/* $out
+          '';
+        };
+
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
           yarn
